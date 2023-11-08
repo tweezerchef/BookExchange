68704cff5b72885327a4d39e865358b7356b7431
tweezerchef <ltomblock@gmail.com>
2023-11-08 16:44:11 -0600

    NytimesReviewFuncWorking

--- src/pages/api/nyTimes.ts
+++ src/pages/api/nyTimes.ts
@@ -1,71 +1,101 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { nyTimesReviewParser } from '../../utils/books/nyTimesReviewParser';
-import prisma from '../../utils/prismaClient';
-
+import type { NextApiRequest, NextApiResponse } from "next";
+import { nyTimesReviewParser } from "../../utils/books/nyTimesReviewParser";
+import prisma from "../../utils/prismaClient";
 
 const key = process.env.NY_TIMES_API_KEY;
 
 type NYTimesResults = {
-    url: string
-}
+  url: string;
+};
 
-interface NYTimesReviewResponse{
-    results: NYTimesResults[]
+interface NYTimesReviewResponse {
+  results: NYTimesResults[];
 }
 
 const setTrue = async (title: string) => {
-       const id = await prisma.books.update({
-        where : {
-            title
-        },
-        data : {
-            nyTimesReq : false,
-        },
-        select : {
-            id : true
-        }
-    })
-    return id
-}
+  const id = await prisma.books.update({
+    where: {
+      title,
+    },
+    data: {
+      nyTimesReq: true,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return id;
+};
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
     const { title } = await prisma.books.findFirst({
-        where : {
-            nyTimesReq: false
-        },
-        select : {
-            title : true
-        }
-    })
-    console.log('title', title);
-    const result =  await fetch(`https://api.nytimes.com/svc/books/v3/reviews.json?title=${title}&api-key=${key}`)
+      where: {
+        nyTimesReq: false,
+      },
+      select: {
+        title: true,
+      },
+    });
+    console.log("title", title);
+    const result = await fetch(
+      `https://api.nytimes.com/svc/books/v3/reviews.json?title=${title}&api-key=${key}`
+    );
     if (!result.ok) {
-        throw new Error(`HTTP error! status: ${result.status}`);
+
+      res.status(429).json({ message: "Error fetching review" });
+      throw new Error(`HTTP error! status: ${result.status}`);
+
     }
-    const parsedResult = await result.json() as NYTimesReviewResponse
+    const parsedResult = (await result.json()) as NYTimesReviewResponse;
     if (!parsedResult.results.length) {
-        // setTrue(title)
-        throw new Error('No results found');
+      void setTrue(title);
+      throw new Error("No results found");
     }
     if (!parsedResult.results[0].url) {
-        // setTrue(title)
-        throw new Error('No results found');
+      void setTrue(title);
+      throw new Error("No results found");
     }
 
-    const {url} = parsedResult.results[0] as {url: string}
+    const { url } = parsedResult.results[0] as { url: string };
     const review = await nyTimesReviewParser(url);
-    console.log('review', review);
+    console.log("review", review);
     if (!review) {
-        // setTrue(title)
-        throw new Error('No review found')
+      void setTrue(title)
+      throw new Error("No review found");
     }
-        const id = await setTrue(title)
-   void  prisma.userBooks.upsert({
-    })
-
-    } catch (error) {
+    const userId = "631fedc2-29f0-4784-aeed-708a0288d5f7";
+    const { id } = await setTrue(title);
+    prisma.userBooks
+      .upsert({
+        where: {
+          userId_bookId: {
+            booksId: id,
+            userId,
+          },
+        },
+        create: {
+          booksId: id,
+          userId,
+          review,
+        },
+        update: {
+          review,
+        },
+      })
+      .then((data) => {
+        console.log("data", data);
+        res.status(200).json({ message: "success" });
+      })
+      .catch((error) => {
         console.error(error);
-        res.status(500).json({ message: 'Error fetching review' });
-    }
+        res.status(500).json({ message: "Error fetching review" });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error fetching review" });
+  }
 }
---
+++ src/utils/dev/NYTDevAPIReapeat.js
@@ -0,0 +1,31 @@
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const callApiMultipleTimes = async (times) => {
+  const endpoint = "http://localhost:3000/api/nyTimes"; // Replace with your actual endpoint
+
+  for (let i = 0; i < times; i++) {
+    try {
+      const response = await fetch(endpoint);
+
+      if (!response.ok) {
+        // If a rate limit error is received, wait longer.
+        if (response.status === 429) {
+          console.error(`Rate limit hit, waiting longer...`);
+          await sleep(12000); // Wait for 12 seconds
+        }
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Response data:", data);
+
+      // Wait for 12 seconds before making the next call
+      await sleep(12000);
+    } catch (error) {
+      console.error("Error calling API:", error);
+    }
+  }
+};
+
+// Call the function with the number of times you want to hit the API
+callApiMultipleTimes(400);
