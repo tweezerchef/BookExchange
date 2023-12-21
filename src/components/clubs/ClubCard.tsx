f8549acac7f388d5e25ec768954fa2067b5c6609
tweezerchef <ltomblock@gmail.com>
2023-12-21 17:47:20 -0600

    added clickey ui to club card

--- src/components/clubs/ClubCard.tsx
+++ src/components/clubs/ClubCard.tsx
@@ -18,9 +18,24 @@ export const ClubCard: FC<ClubProps> = ({ club }) => {
   const { id, name, description, image, memberCount, discussionCount } = club;
   // eslint-disable-next-line @typescript-eslint/naming-convention
   const { Clubs_Books } = club;
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleCardClick = () => {
+    setIsClicked(true);
+    setTimeout(() => {
+      setIsClicked(false);
+    }, 200);
+  };
 
   return (
-    <StyledClubCard>
+    <StyledClubCard
+      onClick={handleCardClick}
+      style={{
+        transform: isClicked ? "scale(0.95)" : "none", // Make it look pressed
+        boxShadow: isClicked ? "inset 0 3px 5px rgba(0, 0, 0, 0.2)" : "none", // Inner shadow for clicked effect
+        // Any other style changes you want to apply when the card is clicked
+      }}
+    >
       <ClubBox image={image} name={name} />
       <BookBox Clubs_Books={Clubs_Books} />
     </StyledClubCard>
