d33abe684372973dd5e89817c1619154e02a7927
tweezerchef <ltomblock@gmail.com>
2023-12-21 14:49:36 -0600

    club card club image loading

--- src/components/clubs/ClubCard.tsx
+++ src/components/clubs/ClubCard.tsx
@@ -1,18 +1,37 @@
 import { FC, useEffect, useState } from "react";
 import { Clubs } from "@prisma/client";
-import { CloseButton } from "../book/bigBookStyles";
+import { Box } from "@mui/material";
+import Image from "next/image";
 
 interface ClubProps {
   club: Clubs;
 }
 
 export const ClubCard: FC<ClubProps> = ({ club }) => {
+  const { id, name, description, image, memberCount, discussionCount } = club;
   const [clubImage, setClubImage] = useState<string>("");
-  useEffect(() => {}, [club]);
+
+  useEffect(() => {
+    const getS3 = async () => {
+      const response = await fetch(`/api/AWS/signedURL?fileNames=${image}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = (await response.json()) as { url: string };
+      const { url } = data;
+      if (url) {
+        setClubImage(url);
+      }
+    };
+
+    getS3();
+  }, [image]);
 
   return (
-    <div>
-      <h1>ClubCard</h1>
-    </div>
+    <Box>
+      <Image src={clubImage} width={200} height={200} alt={name} />
+    </Box>
   );
 };
