88de5bf5fcd35ee2c7028fd86ae698afeddd40d1
Tweezerchef <ltomblock@gmail.com>
2023-12-24 17:24:28 -0500

    calender/date libraries installed need to impliment UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 import { UserProvider } from "../context/context";
 import MuiThemeProvider from "../globalStyles/muiThemeProvider";
 import "../styles/global.css";
@@ -6,12 +9,14 @@ import Layout from "../layout/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
-    <MuiThemeProvider>
-      <UserProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </UserProvider>
-    </MuiThemeProvider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <MuiThemeProvider>
+        <UserProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </UserProvider>
+      </MuiThemeProvider>
+    </LocalizationProvider>
   );
 }
