9a8af56df7c71630bfc243847b8328eac63f1bdc
tweezerchef <ltomblock@gmail.com>
2024-01-05 14:15:56 -0600

    create club api route and neccessary utility functions created, need to test and incorporate with FE

--- src/pages/api/clubs/createClub.ts
+++ src/pages/api/clubs/createClub.ts
@@ -3,6 +3,7 @@ import { convertBase64ToStream } from "../../../utils/convertBase64ToStream";
 import { uploadToS3 } from "../../../utils/s3Upload";
 import { createBookClub } from "../../../utils/clubs/createBookClub";
 import { createClubBook } from "../../../utils/clubs/createClubBook";
+import { findOrCreateBookISBNOnly } from "../../../utils/books/findOrCreateBookISBN";
 
 interface CreateClubRequest {
     bookEndDate: Date;
@@ -14,28 +15,34 @@ interface CreateClubRequest {
     userId: string;
 }
 
-
-
-const handler: NextApiHandler= async(
+const handler: NextApiHandler = async (
     req: NextApiRequest,
     res: NextApiResponse
-  ) => {
-if (req.method !== "POST") {
-    console.error("Method not allowed");
-    res.status(405).json({ message: "Method not allowed" });
+) => {
+    if (req.method !== "POST") {
+        console.error("Method not allowed");
+        res.status(405).json({ message: "Method not allowed" });
+    }
+    try {
+        const { bookEndDate, bookStartDate, clubBookISBN10, clubDescription, clubImage, clubName, userId } = req.body as CreateClubRequest;
+        const base64Data = clubImage.replace(/^data:image\/\w+;base64,/, "");
+        const stream = convertBase64ToStream(base64Data);
+        const fileName = `clubAvi/${clubName}.png`;
+        uploadToS3(stream, fileName);
+
+        const [bookClub, book] = await Promise.all([
+            createBookClub(clubName, clubDescription, fileName, userId),
+            findOrCreateBookISBNOnly(clubBookISBN10)
+        ]);
+
+        const clubId = bookClub.id;
+        const booksId = book.id;
+        await createClubBook(booksId, clubId, bookStartDate, bookEndDate);
+        return  res.status(200).json(clubId)
+    } catch (error) {
+        console.error('An error occurred:', error);
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
 }
-const { bookEndDate, bookStartDate, clubBookISBN10, clubDescription, clubImage, clubName, userId } = req.body as CreateClubRequest;
-const base64Data = clubImage.replace(/^data:image\/\w+;base64,/, "");
-const stream =  convertBase64ToStream(base64Data);
-const fileName = `clubAvi/${clubName}.png`;
-uploadToS3(stream, fileName);
 
-const newClub = await createBookClub(clubName, clubDescription, fileName, userId);
-
-
-
-
-
-
-}
 export default handler
