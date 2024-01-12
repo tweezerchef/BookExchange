3ea2f59ecc1a660d50793c5269adb8a6c349e0ef
tweezerchef <ltomblock@gmail.com>
2024-01-12 17:27:51 -0600

    discussion being fetched

---
+++ src/utils/dev/discussion.mjs
@@ -0,0 +1,14 @@
+import prisma from "../prismaClient";
+
+const fakeData = () => {
+  prisma.discussions.create({
+    data: {
+      title: "Dune Bitch",
+      body: "Exploring the Depths of Dune', 'This is a fascinating discussion about the intricate world and themes of Frank Herbert''s Dune. Let''s dive into the universe of Arrakis, discuss the complex characters, and explore the novel''s profound themes.",
+      clubsId: "55262086-79de-492a-85b1-626b0d44d530",
+      userId: "631fedc2-29f0-4784-aeed-708a0288d5f7",
+      bookTitle: "Dune",
+    },
+  });
+  fakeData();
+};
